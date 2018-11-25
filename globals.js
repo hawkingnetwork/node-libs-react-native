97319d7cef99ff7e883585ca5a79a8c9da4e9731
Duong Le <naviocean@users.noreply.github.com>
2018-11-26 00:40:03 +0700

    add eventsource to global

--- globals.js
+++ globals.js
@@ -1,5 +1,6 @@
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+global.EventSource = require('eventsource');
 
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
